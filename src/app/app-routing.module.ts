2cb3e734877032f07a6fbe64b03ba07c740bd530
Prakhar <agrawalprakhar893@gmail.com>
2023-09-06 15:53:22 +0530

    Added Login/Register Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+// import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  '',component:LandingpageComponent},
+  {
+    path:'login',component:LoginComponent
+  },
+  {
+    path:'signup',component:SignupComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,53 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import {  HttpClientModule } from '@angular/common/http';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule}  from '@angular/material/input'
+
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import { LandingpageComponent } from './pages/landingpage/landingpage.component';
+import { ProfileComponent } from './shared/reusableComponents/profile/profile.component';
+import {MatIconModule} from '@angular/material/icon';
+import { ResetPasswordComponent } from './shared/reusableComponents/reset-password/reset-password.component';
+import { UpdatePasswordComponent } from './shared/reusableComponents/update-password/update-password.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    LandingpageComponent,
+    ProfileComponent,
+
+    ResetPasswordComponent,
+    UpdatePasswordComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
